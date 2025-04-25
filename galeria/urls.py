6c7734078e72b7dada97118e8b731d3fc1823246
Gerardo Rodrigues <gerardofilho56@gmail.com>
2025-04-25 15:57:32 -0300

    mecanismo de busca

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from galeria.views import hello, image
+from galeria.views import buscar, hello, image
 
 
 urlpatterns = [
     path('', hello, name='home'),
-    path('imagem/<int:foto_id>', image, name='imagem')
+    path('imagem/<int:foto_id>', image, name='imagem'),
+    path('buscar/', buscar, name='buscar'),
 ]
