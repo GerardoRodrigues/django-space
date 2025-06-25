12d802f5e86989e4079594b0b3efa98957db7f81
Gerardo Rodrigues <gerardofilho56@gmail.com>
2025-06-25 09:32:59 -0300

    cadastro funcional

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from usuarios.forms import CadastroForms, LoginForms
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -10,4 +12,26 @@ def login(request):
 
 def cadastro(request):
     form = CadastroForms()
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+        
+        if form.is_valid():
+            if form['senha'].value() != form['confirmar_senha'].value():
+                return redirect('cadastro')
+            
+            nome = form['nome_cadastro'].value()
+            email = form['email'].value()
+            senha = form['senha'].value()
+            
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastro')
+            
+            user = User.objects.create_user(
+                username=nome, 
+                email=email, 
+                password=senha
+            )
+            user.save()
+            return redirect('login')
+            
     return render(request, 'usuarios/cadastro.html', {'form': form})
